1abf04bda625016195e20f0be2084da211895f6a
eraldolie <138770365+eraldolie@users.noreply.github.com>
2023-07-06 16:20:45 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def main():
+    st.title("Biodata")
+    st.image("photo.JPG")
+
+    
+    st.write("Nama : Lie, Eraldo Manason")
+    st.write("Umur : 20 tahun")
+    st.write("Tanggal Lahir : 2003 / 01 / 29")
+    st.write("Jenis Kelamin : Laki-laki")
+    st.write("Alamat : Jl. Tambak Mas X No. 123, Semarang, Jawa Tengah")
+    st.write("No Telp : 081225030660")
+    st.write("Email : eraldo.matius@gmail.com")
+    st.write("Status : Belum Menikah")
+
+if __name__ == '__main__':
+    main()
